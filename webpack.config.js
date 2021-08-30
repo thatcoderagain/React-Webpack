e61358c13d5412883825cd1433f0e6de903fa35b
Gaurav <gaurav.officialsites@gmail.com>
2021-08-30 12:00:39 +0530

    Webpack server configured

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('webpack-html-plugin');
 
 module.exports = {
     entry: path.join(__dirname, 'src', 'app.js'),
@@ -9,9 +10,17 @@ module.exports = {
     mode: process.env.NODE_ENV || 'development',
     module: {
         rules: [{
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            loader: ['babel-loader']
+            test: ['/\.(js|jsx)$/'],
+            exclude: [/node_modules/],
+            loader: 'babel-loader'
         }]
-    }
+    },
+    devServer: {
+        static : path.join(__dirname, 'src'),
+    },
+    // plugins: [
+    //     new HtmlWebpackPlugin({
+    //         template: path.join(__dirname, 'src', 'index.html')
+    //     })
+    // ]
 };
